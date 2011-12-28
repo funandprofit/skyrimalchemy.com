f5a71263db0ba3dd1aacfcbf02217c314618afcc
Sebastian von Conrad <sebastian@e-channel.com.au>
2011-12-28 15:02:43 +1030

    Add migrations

--- db/migrate/20111228041907_create_ingredients.rb
+++ db/migrate/20111228041907_create_ingredients.rb
@@ -1,7 +1,9 @@
 class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
-
+      t.string     :name, :game_id, :uesp_link
+      t.integer    :base_value, :null => false, :default => 0
+      t.float      :weight,     :null => false, :default => 0.0
       t.timestamps
     end
   end
--- db/migrate/20111228042626_create_effects.rb
+++ db/migrate/20111228042626_create_effects.rb
@@ -1,7 +1,8 @@
 class CreateEffects < ActiveRecord::Migration
   def change
     create_table :effects do |t|
-
+      t.string     :name, :game_id, :uesp_link
+      t.boolean    :positive, :null => false, :default => true
       t.timestamps
     end
   end
