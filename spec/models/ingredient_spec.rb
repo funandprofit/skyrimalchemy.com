43d0d3b6e42cc2db3613c8474e7f3e22f0937174
Sebastian von Conrad <sebastian@e-channel.com.au>
2011-12-29 16:09:15 +1030

    Add specs for matching ingredients

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,7 +2,44 @@ require 'spec_helper'
 
 describe Ingredient do
   it 'creates a slug for the ingredient' do
-    effect = Effect.make(:name => 'Some effect')
-    effect.slug.should == 'some-effect'
+    ingredient = Ingredient.make(:name => 'Some ingredient')
+    ingredient.slug.should == 'some-ingredient'
+  end
+
+  context 'matching ingredients' do
+    before do
+      @foo = Ingredient.make(:name => 'Foo')
+      @bar = Ingredient.make(:name => 'Bar')
+      @baz = Ingredient.make(:name => 'Baz')
+
+      @effects = 3.times.map { Effect.make }
+    end
+
+    it 'finds matching ingredients for an ingredient' do
+      @foo.effects << @effects.first
+      @bar.effects << @effects.first
+      @baz.effects << @effects.last
+
+      @foo.ingredient_matchers.should     include(@bar)
+      @foo.ingredient_matchers.should_not include(@baz)
+    end
+
+    it 'finds ideal matching ingredients for an ingredient' do
+      @foo.effects = @effects
+      @bar.effects = @effects[0, 2]
+      @baz.effects = @effects[0, 1]
+
+      @foo.matching_ingredients.should     include(@bar)
+      @foo.matching_ingredients.should_not include(@baz)
+    end
+
+    it 'sorts matching ingredients based of number of common effects' do
+      @foo.effects = @effects
+      @bar.effects = @effects[0, 2]
+      @baz.effects = @effects
+
+      @foo.matching_ingredients.should     == [@baz, @bar]
+      @foo.matching_ingredients.should_not == [@bar, @baz]
+    end
   end
 end
