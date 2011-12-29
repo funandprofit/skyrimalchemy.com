2729fb68326f135bc0da2fc430b34fc3f0296e2f
Sebastian von Conrad <sebastian@e-channel.com.au>
2011-12-29 16:18:04 +1030

    Add proper page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def page_title
+    case
+      when @ingredients.present?
+        'Skyrim Alchemy Ingredients'
+      when @ingredient.present?
+        "#{@ingredient.name} - Skyrim Alchemy Ingredients"
+      when @effects.present?
+        'Skyrim Alchemy Effects'
+      when @effect.present?
+        "#{@effect.name} - Skyrim Alchemy Effects"
+      else
+        'Skyrim Alchemy Ingredients'
+    end
+  end
 end
