2b1130259127fb939a4ba4e454bcbfd70996763d
István Huszár <istvan@e-channel.com.au>
2012-01-17 19:01:30 +1030

    Add console.log wrapper

---
+++ app/assets/javascripts/plugins/log.js
@@ -0,0 +1,9 @@
+/*
+************
+console.log wrapper
+usage: log('inside coolFunc',this,arguments);
+http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+************
+*/
+
+window.log=function(){log.history=log.history||[];log.history.push(arguments);if(this.console){console.log(Array.prototype.slice.call(arguments))}};
