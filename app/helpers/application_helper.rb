1cb35d719c3e154f844410da6546e97d60ea6bac
István Huszár <istvan@e-channel.com.au>
2012-01-28 21:43:45 +1030

    Add helpers for tooltips and main menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,34 @@ module ApplicationHelper
         "#{@effect.name} - Skyrim Alchemy Potion Effects"
     end
   end
+
+  def menu_class(item)
+    klass  = []
+    klass << 'menu-wrapper'
+    klass << item.to_s
+    klass << 'active' if params[:controller] == item.to_s
+
+    klass.join(' ')
+  end
+
+  def headline(obj)
+    content_tag(:h3, obj.name + uesp_link(obj))
+  end
+
+  def uesp_link(obj)
+    link_to '', obj.uesp_link, class: 'uesp', title: "See #{obj.name} on UESP"
+  end
+
+  def tooltip(ingredient)
+    tooltip = [headline(ingredient), *ingredient.effects.map(&:name).join('<br>')]
+
+    tooltip.join.html_safe
+  end
+
+  # (#{(ingredient.effects & @ingredient.effects).map(&:name).join(', ')})
+  def effect_list(ingredient)
+    (ingredient.effects & @ingredient.effects).inject([]) do |a, e|
+      a << link_to(e.name, ingredient_path(e), class: 'effect tooltip')
+    end.join(', ').html_safe
+  end
 end
