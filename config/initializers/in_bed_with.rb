fc49aece734cf9fde3e9c1af41948bbf83f9b8e7
Sebastian von Conrad <sebastian@e-channel.com.au>
2012-01-29 00:30:24 +1030

    Add analytics

--- config/initializers/in_bed_with.rb
+++ config/initializers/in_bed_with.rb
@@ -1,4 +1,5 @@
 InBedWith.config do |config|
+  config.analytics_property_id = 'UA-28186953-2'
   config.gauges_site_id = '4f095265613f5d3b5a000001'
   config.adsense_client = 'ca-pub-5930429795374127'
 end
