a995752bd07d4aad3ed0e41b13436d55a09c50f2
Sebastian von Conrad <sebastian@e-channel.com.au>
2012-01-29 14:34:53 +1030

    Increase effects#index performance

--- app/controllers/effects_controller.rb
+++ app/controllers/effects_controller.rb
@@ -2,4 +2,9 @@ class EffectsController < InheritedResources::Base
   defaults :finder => :find_by_slug
 
   actions :index, :show
+
+  private
+    def collection
+      @effects ||= end_of_association_chain.includes(:ingredients)
+    end
 end
